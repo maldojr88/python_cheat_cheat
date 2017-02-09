664f253fad86cf2507f18ede9c3b07754ed2d769
maldojr88 <maldojr88@gmail.com>
2017-02-09 17:29:47 -0500

    Adding functions

--- python_cheat_cheat.py
+++ python_cheat_cheat.py
@@ -1,15 +1,49 @@
 """
+Header
+"""
+
+"""
     Interpreter
 
         sourcecode => python bytecode
 """
-
-#the interpreter first compilessoure files into python bytecode .pyc
+# the interpreter first compiles source files into python bytecode .pyc
 bytecode = compile('x=2\nprint "X is", x',"fake_module", "exec")
-#this is the bytecode representation
+# this is the bytecode representation
 print [ord(byte) for byte in bytecode.co_code]
-#run a piece of code
+
+"""
+    Interpreter
+        pyc ==> execute
+"""
+
+# run a piece of code
 print eval(bytecode)
 
+'''
+    Virtual Machine Concepts
+
+        + Python VM is a stack machine
+        + Garbage collection is done by reference counting
+        + Variables aren't really added to the stack, just a reference
+'''
+
+
+'''
+    Functions
+
+        + Internally the just extend PyObject
+        + In essence contain a code object with the byte code and variables surrounding the "environment"
+        + When you call a function, basically a new frame is created with code to execute, global + local variables
+        + Function objects are created when <def> is encountered
+
+'''
+
+
+def my_func(x):
+    y = x + 1
+    print y
+
+print dir(my_func)
 
 
